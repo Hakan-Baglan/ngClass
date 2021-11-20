b58a03b0a46a5d506a4c78ba430891e79eb99e0f
Hakan Bağlan <baglanhakan42@gmail.com>
2021-11-20 20:34:02 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngproje';
+
+  kitaplar:any = [
+    { 
+      name:"Kör Oğlu", okundumu:true, favorite:true
+    },
+    { 
+      name:"Marslı", okundumu:false, favorite:true
+    },
+    { 
+      name:"Aya Yolculuk", okundumu: true, favorite:false
+    }
+  ]
+
 }
